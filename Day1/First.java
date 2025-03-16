e483979158d1c3bc7cc104108ac1732699f910a7
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2025-03-16 10:43:50 +0000

    Answers for 1 to 4

--- Day1/First.java
+++ Day1/First.java
@@ -1,1 +1,26 @@
+public class First {
+    public static void main(String []args)
+    {
+        System.out.println("Hello  World");
 
+        System.out.println("\n");
+        System.out.println("1 to 10 numbers:");
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+        System.out.println("\n");
+        System.out.println("10 to 1 reverse numbers:");
+        for(int i=10;i>0;i--){
+            System.out.println(i);
+        }
+        System.out.println("\n");
+        System.out.println("1 to 20 Odd numbers:");
+        for (int i=1;i<=20;i++){
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+        System.out.println("\n");
+        
+    }
+}
